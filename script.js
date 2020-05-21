50e1ffbc1aaaf7619a13f31c1c6f13c90c510349
Lcdevi <caillauxlaura@gmail.com>
2020-05-21 13:17:24 +0200

    add 2+4

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-/*
-let nbrA = Math.floor(prompt("Choose a number between 1 and 4"));
-let nbrB = Math.floor(prompt("Choose a number between 1 and 4"));
-let operator = prompt("choose an operator");
+let twoBtn = document.getElementById('twobtn');
+let fourBtn = document.getElementById('fourbtn');
 
-console.log(nbrA, nbrB, operator);
+console.log(twoBtn);
+console.log(fourBtn);
 
-if (operator === "+") {
- let sum = nbrA + nbrB;
- console.log(sum);
-}
-*/
-
-let twoBtn = document.querySelector("#twobtn");
-let fourBtn = document.querySelector("#fourbtn");
-let plusBtn = document.querySelector("#plusbtn");
-let egalBtn = document.querySelector("#egalbtn");
-let nbrB = 0;
-let nbrA = 0;
-
-twoBtn.addEventListener("click", function(){
-  nbr = 2;
-  console.log(nbr);
+twoBtn.addEventListener('click', function(e){
+  let nbrA = 2;
+  console.log(nbrA);
 })
 
-fourBtn.addEventListener("click", function(){
-  nbr = 4;
-  console.log(nbr);
+fourBtn.addEventListener('click', function(e){
+  let nbrB = 4;
+  console.log(nbrB);
 })
 
-plusBtn.addEventListener("click", function(){
-  operator = "+";
-  newNbrA = nbr;
-  newNbrB = nbr;
-  console.log(operator);
-})
-
-egalBtn.addEventListener("click", function(){
-  let sum = add(newNbrA, newNbrB);
-  console.log(sum);
-
-})
-
-function add(a,b){
-  return a + b;
+function add() {
+  nbrA + nbrB;
 }
