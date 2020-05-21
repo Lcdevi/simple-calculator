c7850fb362df5cedf76390ce18a79fc5bc516d0b
Lcdevi <caillauxlaura@gmail.com>
2020-05-21 17:40:28 +0200

    add number 2 and 4

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 let twoBtn = document.getElementById('twobtn');
 let fourBtn = document.getElementById('fourbtn');
+let plusBtn = document.getElementById('plusbtn');
+let displayjs = document.getElementById('display');
+let egalBtn = document.getElementById('egalbtn');
 
 console.log(twoBtn);
 console.log(fourBtn);
+console.log(displayjs);
+let nbrA = 0;
+let nbrB = 0
+let displayValue = "";
 
+//quand on clique sur 2, le 2 apparait dans le display.
+//Aussi display value vaut 2
 twoBtn.addEventListener('click', function(e){
-  let nbrA = 2;
-  console.log(nbrA);
+  displayValue = displayjs.textContent = displayValue + 2;
 })
 
+//quand on clique sur 4, le 4 apparait dans le display.
+//Aussi display value vaut 4
 fourBtn.addEventListener('click', function(e){
-  let nbrB = 4;
-  console.log(nbrB);
+  displayValue = displayjs.textContent = displayValue + 4;
 })
 
-function add() {
-  nbrA + nbrB;
-}
+plusBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + "+";
+})
+
+egalBtn.addEventListener('click', function(e){
+  console.log(displayValue);
+
+  let array = displayValue.split("+");
+    console.log(array);
+
+      let nbx = Math.floor(array[0]);
+      let nby = Math.floor(array[1]);
+      let result = nbx + nby;
+      console.log(nbx, nby);
+      console.log(result);
+
+
+})
