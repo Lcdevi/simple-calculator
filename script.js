3a7ac4ffab69dd99000eccd6cf957a108027ee68
Lcdevi <caillauxlaura@gmail.com>
2020-05-22 19:03:30 +0200

    eval function. bug with result and operator

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+let zeroBtn = document.getElementById('zerobtn');
+let oneBtn = document.getElementById('onebtn');
 let twoBtn = document.getElementById('twobtn');
+let threeBtn = document.getElementById('threebtn');
 let fourBtn = document.getElementById('fourbtn');
 let plusBtn = document.getElementById('plusbtn');
+let minusBtn = document.getElementById('minusbtn');
+let divideBtn = document.getElementById('dividebtn');
 let displayjs = document.getElementById('display');
+let resultjs = document.getElementById('result');
 let egalBtn = document.getElementById('egalbtn');
+let clearBtn = document.getElementById('clearbtn');
 
-console.log(twoBtn);
-console.log(fourBtn);
-console.log(displayjs);
-let nbrA = 0;
-let nbrB = 0
+//console.log(twoBtn);
+//console.log(fourBtn);
+//console.log(displayjs);
+//let nbrA = 0;
+//let nbrB = 0
 let displayValue = "";
+let resultValue = "";
+
+zeroBtn.addEventListener('click', function(e){
+  if (displayValue > 0) {
+    displayValue = displayjs.textContent = displayValue + 0;
+  }
+})
+
+oneBtn.addEventListener('click', function(e){
+    displayValue = displayjs.textContent = displayValue + 1;
+})
 
 //quand on clique sur 2, le 2 apparait dans le display.
 //Aussi display value vaut 2
@@ -17,27 +35,40 @@ twoBtn.addEventListener('click', function(e){
   displayValue = displayjs.textContent = displayValue + 2;
 })
 
+threeBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + 3;
+})
+
 //quand on clique sur 4, le 4 apparait dans le display.
 //Aussi display value vaut 4
 fourBtn.addEventListener('click', function(e){
-  displayValue = displayjs.textContent = displayValue + 4;
+  if (resultValue > 0 || resultValue < 0) {
+    displayValue = displayjs.textContent = resultValue + 4;
+  } else {displayValue = displayjs.textContent = displayValue + 4;}
 })
 
 plusBtn.addEventListener('click', function(e){
-  displayValue = displayjs.textContent = displayValue + "+";
+  if (resultValue === "") {
+    displayValue = displayjs.textContent = displayValue + " + ";
+  } else if (resultValue > 0 || resultValue < 0) {
+    displayValue = displayjs.textContent = resultValue + " + ";
+  }
+})
+
+minusBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + " - ";
+})
+
+divideBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + " / ";
 })
 
 egalBtn.addEventListener('click', function(e){
-  console.log(displayValue);
+  resultValue = eval(displayValue);
+  displayValue = displayjs.textContent = displayValue + " = " + resultValue;
+  displayValue = "";
+})
 
-  let array = displayValue.split("+");
-    console.log(array);
-
-      let nbx = Math.floor(array[0]);
-      let nby = Math.floor(array[1]);
-      let result = nbx + nby;
-      console.log(nbx, nby);
-      console.log(result);
-
-
+clearBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = "";
 })
