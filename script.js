5d32d912c19ee0016d217866714fa3d3d7721854
Lcdevi <caillauxlaura@gmail.com>
2020-05-27 13:06:32 +0200

    trying without eval()

--- script.js
+++ script.js
@@ -1,46 +1,74 @@
-/*
-let nbrA = Math.floor(prompt("Choose a number between 1 and 4"));
-let nbrB = Math.floor(prompt("Choose a number between 1 and 4"));
-let operator = prompt("choose an operator");
+let zeroBtn = document.getElementById('zerobtn');
+let oneBtn = document.getElementById('onebtn');
+let twoBtn = document.getElementById('twobtn');
+let threeBtn = document.getElementById('threebtn');
+let fourBtn = document.getElementById('fourbtn');
+let plusBtn = document.getElementById('plusbtn');
+let minusBtn = document.getElementById('minusbtn');
+let divideBtn = document.getElementById('dividebtn');
+let displayjs = document.getElementById('display');
+let resultjs = document.getElementById('result');
+let egalBtn = document.getElementById('egalbtn');
+let clearBtn = document.getElementById('clearbtn');
 
-console.log(nbrA, nbrB, operator);
+//console.log(twoBtn);
+//console.log(fourBtn);
+//console.log(displayjs);
+//let nbrA = 0;
+//let nbrB = 0
+let displayValue = "";
+let resultValue = "";
 
-if (operator === "+") {
- let sum = nbrA + nbrB;
- console.log(sum);
-}
-*/
-
-let twoBtn = document.querySelector("#twobtn");
-let fourBtn = document.querySelector("#fourbtn");
-let plusBtn = document.querySelector("#plusbtn");
-let egalBtn = document.querySelector("#egalbtn");
-let nbrB = 0;
-let nbrA = 0;
-
-twoBtn.addEventListener("click", function(){
-  nbrA = 2;
-  console.log(nbrA);
+zeroBtn.addEventListener('click', function(e){
+  if (displayValue > 0) {
+    displayValue = displayjs.textContent = displayValue + 0;
+  }
 })
 
-fourBtn.addEventListener("click", function(){
-  nbrB = 4;
-  console.log(nbrB);
+oneBtn.addEventListener('click', function(e){
+    displayValue = displayjs.textContent = displayValue + 1;
 })
 
-plusBtn.addEventListener("click", function(){
-  operator = "+";
-  newNbrA = nbrA;
-  newNbrB = nbrB;
-  console.log(operator);
+//quand on clique sur 2, le 2 apparait dans le display.
+//Aussi display value vaut 2
+twoBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + 2;
 })
 
-egalBtn.addEventListener("click", function(){
-  let sum = newNbrA + newNbrB;
-  console.log(sum);
-
+threeBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + 3;
 })
 
-function add(a,b){
-  return a + b;
-}
+//quand on clique sur 4, le 4 apparait dans le display.
+//Aussi display value vaut 4
+fourBtn.addEventListener('click', function(e){
+  if (resultValue > 0 || resultValue < 0) {
+    displayValue = displayjs.textContent = resultValue + 4;
+  } else {displayValue = displayjs.textContent = displayValue + 4;}
+})
+
+plusBtn.addEventListener('click', function(e){
+  if (resultValue === "") {
+    displayValue = displayjs.textContent = displayValue + " + ";
+  } else if (resultValue > 0 || resultValue < 0) {
+    displayValue = displayjs.textContent = resultValue + " + ";
+  }
+})
+
+minusBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + " - ";
+})
+
+divideBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = displayValue + " / ";
+})
+
+egalBtn.addEventListener('click', function(e){
+  resultValue = eval(displayValue);
+  displayValue = displayjs.textContent = displayValue + " = " + resultValue;
+  displayValue = "";
+})
+
+clearBtn.addEventListener('click', function(e){
+  displayValue = displayjs.textContent = "";
+})
