38647506d0c2da37e5a492dc1bc86d1ecc3e8673
Lcdevi <caillauxlaura@gmail.com>
2020-05-28 15:40:42 +0200

    fix bug with add, subtract, multiply, egal, clicknumber functions

--- script.js
+++ script.js
@@ -7,158 +7,62 @@ let minusBtn = document.getElementById('minusbtn');
 let multBtn = document.getElementById('multbtn');
 let displayjs = document.getElementById('display');
 let egalBtn = document.getElementById('egalbtn');
-
-console.log(twoBtn);
-console.log(fourBtn);
-console.log(displayjs);
-let nbrA = 0;
-let nbrB = 0
 let displayValue = "";
 let temporaryResult = "";
 let array = 0;
 let operator = "";
 let len = "";
 
-// ***** 1 BUTTON *****
 oneBtn.addEventListener('click', function(e){
-  displayValue = displayValue + "1";
-//  temporaryResult = displayValue;
-  displayjs.textContent = displayValue;
-  console.log("displayValue : " + displayValue);
-  console.log("operateur est : " + operator);
+  clickNumber("1");
 })
 
-// ***** 2 BUTTON *****
 twoBtn.addEventListener('click', function(e){
-  displayValue = displayValue + "2";
-  displayjs.textContent = displayValue;
-  console.log("displayValue : " + displayValue);
-  console.log("operateur est : " + operator);
+  clickNumber("2");
 })
 
-// ***** 3 BUTTON *****
 threeBtn.addEventListener('click', function(e){
-  displayValue = displayValue + "3";
-  displayjs.textContent = displayValue;
-  console.log("displayValue : " + displayValue);
-  console.log("operateur est : " + operator);
+  clickNumber("3");
 })
 
-// ***** 4 BUTTON *****
 fourBtn.addEventListener('click', function(e){
-  displayValue = displayValue + "4";
-  displayjs.textContent = displayValue;
-  console.log("displayValue : " + displayValue);
-  console.log("operateur est : " + operator);
+  clickNumber("4");
 })
 
-// ***** + BUTTON *****
 plusBtn.addEventListener('click', function(e){
   egal();
   operator = "+";
   console.log("operateur est : " + operator);
-  displayValue = displayValue + " + " + temporaryResult;
-  console.log("displayValue : " + displayValue);
-
-  array = displayValue.split(" ");
-    console.log(array);
-    len = array.length;
-    for (i = 0 ; i < len; i++) {
-/*
-      if (array[i] == "+") {
-        temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-        displayjs.textContent = temporaryResult;
-        console.log("temporaryResult : " + temporaryResult);
-        console.log("displayValue : " + displayValue);
-      }
-      */
-      if (array[i] == "+") {
-        temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-        displayjs.textContent = temporaryResult;
-        console.log("displayValue : " + displayValue);
-      //  temporaryResult = displayValue;
-        displayValue = "";
-        console.log("temporaryResult : " + temporaryResult);
-        console.log("displayValue : " + displayValue);
-
-      }
-      //else {
-        //displayjs.textContent = displayValue;
-        //console.log("displayValue : " + displayValue);
-      //}
-
-    }
-
+  add();
 })
 
-// ***** - BUTTON *****
 minusBtn.addEventListener('click', function(e){
   egal();
   operator = "-";
   console.log("operateur est : " + operator);
-  displayValue = displayValue + " - " + temporaryResult;
-  console.log("displayValue : " + displayValue);
-
-  array = displayValue.split(" ");
-    console.log(array);
-    len = array.length;
-    for (i = 0 ; i < len; i++) {
-/*
-      if (array[i] == "+") {
-        temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-        displayjs.textContent = temporaryResult;
-        console.log("temporaryResult : " + temporaryResult);
-        console.log("displayValue : " + displayValue);
-      }
-      */
-      if (array[2] == "") {
-        temporaryResult = Math.floor(array[0]);
-        displayjs.textContent = temporaryResult;
-        console.log("displayValue : " + displayValue);
-      //  temporaryResult = displayValue;
-        displayValue = "";
-        console.log("temporaryResult : " + temporaryResult);
-        console.log("displayValue : " + displayValue);
-
-      } else {
-        temporaryResult = Math.floor(array[2]) - Math.floor(array[0]);
-        displayjs.textContent = temporaryResult;
-        console.log("displayValue : " + displayValue);
-      //  temporaryResult = displayValue;
-        displayValue = "";
-        console.log("temporaryResult : " + temporaryResult);
-        console.log("displayValue : " + displayValue);
-
-      }
-      //else {
-        //displayjs.textContent = displayValue;
-        //console.log("displayValue : " + displayValue);
-      //}
-    }
+  subtract();
 })
-
-
-
-// ***** X BUTTON *****
 
 multBtn.addEventListener ('click', function(e){
+  egal();
   operator = "*";
+  console.log("operateur est : " + operator);
   multiply();
 })
-
-
-
-// ***** = BUTTON *****
 
 egalBtn.addEventListener('click', function(e){
     console.log(operator);
     egal();
+    operator = "";
 })
 
-
-
-
-
+function clickNumber(numberValue) {
+  displayValue = displayValue + numberValue;
+  displayjs.textContent = displayValue;
+    if (operator == "") {
+      temporaryResult = "";
+    }
+}
 
 function egal() {
   if (operator == "+") {
@@ -176,13 +80,11 @@ function add() {
   console.log(array);
   len = array.length;
     for (i = 0 ; i < len; i++) {
-  //    if (array[i] == "+") {
         temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
         displayjs.textContent = temporaryResult;
         displayValue = "";
         console.log("temporaryResult : " + temporaryResult);
         console.log("displayValue : " + displayValue);
-  //    }
     }
 }
 
@@ -192,7 +94,13 @@ function subtract() {
   console.log(array);
   len = array.length;
     for (i = 0 ; i < len; i++) {
-      if (array[2] == "" || array[0] == "") {
+      if (array[2] == "") {
+        temporaryResult = Math.floor(array[0]);
+        displayjs.textContent = temporaryResult;
+        displayValue = "";
+        console.log("temporaryResult : " + temporaryResult);
+        console.log("displayValue : " + displayValue);
+      } else if (array[0] == "") {
         temporaryResult = Math.floor(array[2]);
         displayjs.textContent = temporaryResult;
         displayValue = "";
@@ -235,108 +143,3 @@ function multiply() {
       }
     }
 }
-/*
-egalBtn.addEventListener('click', function egal(e){
-  console.log("displayValue : " + displayValue);
-
-   array = displayValue.split(" ");
-    console.log(array);
-//    console.log("array 0 nest pas un nombre :" + isNaN(array[1]));
-
-
-    let len = array.length;
-    for (i = 0 ; i < len; i++) {
-      if (array[i] == "+") {
-        temporaryResult = Math.floor(array[i-1]) + Math.floor(array[i+1]);
-        displayjs.textContent = temporaryResult;
-        console.log("temporaryResult : " +temporaryResult);
-      }
-
-//      else {let mathNumber = Math.floor(array[i]);
-//      console.log(mathNumber);
-//    console.log("mathNumber nest pas un nombre :" + isNaN(mathNumber));}
-
-    }
-
-
-//      let nbx = Math.floor(array[0]);
-//      let nby = Math.floor(array[2]);
-//      let result = nbx + nby;
-//      console.log(nbx, nby);
-//      console.log(result);
-
-
-})
-*/
-
-
-egal0Btn.addEventListener('click', function(e){
-
-    console.log("operateur est : " + operator);
-
-    if (operator == "+") {
-    displayValue = displayValue + " + " + temporaryResult;
-    console.log("displayValue : " + displayValue);
-
-    array = displayValue.split(" ");
-      console.log(array);
-      let len = array.length;
-      for (i = 0 ; i < len; i++) {
-  /*
-        if (array[i] == "+") {
-          temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-          displayjs.textContent = temporaryResult;
-          console.log("temporaryResult : " + temporaryResult);
-          console.log("displayValue : " + displayValue);
-        }
-        */
-        if (array[i] == "+") {
-          temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-          displayjs.textContent = temporaryResult;
-          console.log("displayValue : " + displayValue);
-        //  temporaryResult = displayValue;
-          displayValue = "";
-          console.log("temporaryResult : " + temporaryResult);
-          console.log("displayValue : " + displayValue);
-
-        }
-        //else {
-          //displayjs.textContent = displayValue;
-          //console.log("displayValue : " + displayValue);
-        //}
-      }
-    } else if (operator == "-") {
-    displayValue = displayValue + " - " + temporaryResult;
-    console.log("displayValue : " + displayValue);
-
-    array = displayValue.split(" ");
-      console.log(array);
-      let len = array.length;
-      for (i = 0 ; i < len; i++) {
-  /*
-        if (array[i] == "+") {
-          temporaryResult = Math.floor(array[0]) + Math.floor(array[2]);
-          displayjs.textContent = temporaryResult;
-          console.log("temporaryResult : " + temporaryResult);
-          console.log("displayValue : " + displayValue);
-        }
-        */
-        if (array[i] == "-") {
-          temporaryResult = Math.floor(array[2]) - Math.floor(array[0]);
-          displayjs.textContent = temporaryResult;
-          console.log("displayValue : " + displayValue);
-        //  temporaryResult = displayValue;
-          displayValue = "";
-          console.log("temporaryResult : " + temporaryResult);
-          console.log("displayValue : " + displayValue);
-
-        }
-        //else {
-          //displayjs.textContent = displayValue;
-          //console.log("displayValue : " + displayValue);
-        //}
-      }
-
-    }
-
-})
